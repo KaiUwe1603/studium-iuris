187a75a2bd19dffb4919b1efa2e143940644f843
KaiUwe1603 <time.alb@gmail.com>
2023-01-04 15:16:43 +0100

    Javascript clean

--- myScript.js
+++ myScript.js
@@ -1,20 +1,4 @@
-//Collapsibles
-var coll = document.getElementsByClassName("collapsible");
-var i;
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-        content.style.maxHeight = null;
-    } else {
-        content.style.maxHeight = content.scrollHeight + "100px";
-    } 
-    });
-}
-
-
-//Test
+//Menu
 function myFunction() {
     var x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
@@ -23,7 +7,5 @@ function myFunction() {
       x.className = "topnav";
     }
   }
-
-  //hallo
 
 
