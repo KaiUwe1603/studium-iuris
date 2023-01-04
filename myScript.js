703574ef8b3b01cd0d8b2cff59bbd3fd136104dd
KaiUwe1603 <time.alb@gmail.com>
2023-01-04 14:43:52 +0100

    Weiter geht es

---
+++ myScript.js
@@ -0,0 +1,27 @@
+//Collapsibles
+var coll = document.getElementsByClassName("collapsible");
+var i;
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+        content.style.maxHeight = null;
+    } else {
+        content.style.maxHeight = content.scrollHeight + "100px";
+    } 
+    });
+}
+
+
+//Test
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
